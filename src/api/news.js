60e9c352d1ce1bc98fa032c8dd0f37c5dad6b4c3
YT <guoyanting0919@gmail.com>
2020-12-16 15:54:07 +0800

    init

--- src/api/news.js
+++ src/api/news.js
@@ -1,40 +1,10 @@
 import request from "@/utils/request";
 
-export function load(params) {
+// 獲取管理員列表
+export function getList(params) {
   return request({
     url: "/newss/load",
     method: "get",
     params,
-  });
-}
-export function get(params) {
-  return request({
-    url: "/newss/get",
-    method: "get",
-    params,
-  });
-}
-
-export function add(data) {
-  return request({
-    url: "/newss/add",
-    method: "post",
-    data,
-  });
-}
-
-export function update(data) {
-  return request({
-    url: "/newss/update",
-    method: "post",
-    data,
-  });
-}
-
-export function remove(data) {
-  return request({
-    url: "/newss/delete",
-    method: "post",
-    data,
   });
 }
---
+++ src/views/member/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="flex-column">
+    <sticky :className="'sub-navbar'">
+      <div class="filter-container">
+        <permission-btn
+          size="mini"
+          moduleName="modulemanager"
+          v-on:btn-event="onBtnClicked"
+        ></permission-btn>
+      </div>
+    </sticky>
+    <div class="app-container flex-item">
+      <Title title="最新消息管理"></Title>
+      <div class="bg-white" style="height: calc(100% - 50px)">
+        <el-table
+          ref="mainTable"
+          :data="list"
+          v-loading="listLoading"
+          border
+          fit
+          highlight-current-row
+          style="width: 100%"
+          height="calc(100% - 52px)"
+          @row-click="rowClick"
+          @selection-change="handleSelectionChange"
+        >
+          <el-table-column
+            align="center"
+            type="selection"
+            width="55"
+          ></el-table-column>
+          <el-table-column min-width="80px" :label="'名稱'">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column min-width="80px" :label="'年紀'">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column min-width="80px" :label="'生日'">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column min-width="80px" :label="'消息'">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column min-width="80px" :label="'地點'">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column min-width="80px" :label="'時間'">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column min-width="80px" :label="'性別'">
+            <template slot-scope="scope">
+              <span>{{ scope.row.name }}</span>
+            </template>
+          </el-table-column>
+        </el-table>
+        <pagination
+          v-show="total > 0"
+          :total="total"
+          :page.sync="listQuery.page"
+          :limit.sync="listQuery.limit"
+          @pagination="handleCurrentChange"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sticky from "@/components/Sticky";
+import Title from "@/components/ConsoleTableTitle";
+import permissionBtn from "@/components/PermissionBtn";
+import Pagination from "@/components/Pagination";
+export default {
+  name: "news",
+  components: { Sticky, Title, permissionBtn, Pagination },
+  data() {
+    return {
+      list: [], // 菜單列表
+      total: 10,
+      listLoading: false,
+      listQuery: {
+        page: 1,
+        limit: 20,
+        orgId: "",
+        key: undefined,
+      },
+    };
+  },
+  methods: {
+    onBtnClicked(domId) {
+      this.$cl(domId);
+    },
+    rowClick() {},
+    handleSelectionChange() {},
+    handleCurrentChange() {},
+  },
+};
+</script>
+
+<style>
+</style>
